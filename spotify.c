db9887efce14d43059afc7765c6b64e10ed40d6c
GabrielePiumatto <71849280+GabrielePiumatto@users.noreply.github.com>
2020-11-05 10:57:47 +0100

    Add files via upload

---
+++ spotify.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 100
+#define NSONG 1000
+#define BSIZE 1000
+
+typedef struct SCanzone{
+	int numero;
+	char titolo[MAX];
+	char artista[MAX];
+}Canzone;
+
+Canzone playlist[NSONG];
+
+int main()
+{
+    FILE* fileptr;
+    char riga[BSIZE];
+    int counter = 0;
+
+    fileptr = fopen("lettura.csv", "r");
+
+    if(fileptr)
+    {
+
+        while(fgets(riga, BSIZE, f))
+	    {
+		    field=strtok(riga,",");
+		    canzoni[counter].numero=atoi(field);
+		    field=strtok(NULL, ",");
+		    canzoni[counter].titolo=strdup(field);
+		    field=strtok(NULL, ",");
+		    canzoni[counter].artista=strdup(field);
+
+		    counter++;
+	    }
+    }
+
+    else
+    {
+        printf("Errore file!");
+    }
+    
+    fclose(fileptr);
+
+    return 0;
+}
